76fad0e4648f56214ca8256c7e49a513ac161aca
郭怀 <2085187652@qq.com>
2018-07-16 11:53:22 +0800

    grf
    gtfr

--- src/p1/Search.java
+++ src/p1/Search.java
@@ -26,33 +26,33 @@ public class Search extends HttpServlet {
 			throws ServletException, IOException {
 		
 		response.setContentType("text/plain;charset=utf-8");
-		String student_id = request.getParameter("id");
-		System.out.println("id:" + student_id);
+		String user = request.getParameter("user");
+		//System.out.println("id:" + student_id);
 		
 		try {
 			////////////
 			// do something
 			////////////
 			DBConnection db = new DBConnection();
-			ResultSet rs = db.executeQuery("select * from qusetions where id = '" + student_id +"'");
+			ResultSet rs = db.executeQuery("select * from users where username = '" +user+"'");
 			
 
 			String id = "";
-			String q = "";
-			String c1 = "";
-			String c2 = "";
-			String c3 = "";
-			String c4="";
-			String an="";
+			String score = "";
+			String username = "";
+			String password = "";
+			//String c3 = "";
+			//String c4="";
+			//String an="";
 			
 			while(rs.next()){
 				id=rs.getString(1);
-				q=rs.getString(2);
-				c1=rs.getString(3);
-				c2=rs.getString(4);
-				c3=rs.getString(5);
-				c4=rs.getString(6);
-				an=rs.getString(7);
+				score=rs.getString(2);
+				username=rs.getString(3);
+				password=rs.getString(4);
+//				c3=rs.getString(5);
+//				c4=rs.getString(6);
+//				an=rs.getString(7);
 			}
 			db.close();
 			
@@ -61,12 +61,12 @@ public class Search extends HttpServlet {
 			JSONObject obj = new JSONObject();
 			
 			obj.put("id", id);
-			obj.put("q", q);
-			obj.put("c1", c1);
-			obj.put("c2", c2);
-			obj.put("c3", c3);
-			obj.put("c4", c4);
-			obj.put("an", an);
+			obj.put("score", score);
+			obj.put("username", username);
+			obj.put("password", password);
+//			obj.put("c3", c3);
+//			obj.put("c4", c4);
+//			obj.put("an", an);
 			
 			System.out.println(obj.toString());
 			out.print(obj.toString());
